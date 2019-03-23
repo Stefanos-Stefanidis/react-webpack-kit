a9c5a8211cd307ba60991e3af8a2c0cb60413c6e
stef <s.stevenidis@gmail.com>
2019-03-24 01:14:20 +0200

    add axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: [],
+      isLoading: true,
+    }
+
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then(response => {
+        console.log(response.data)
+        this.setState({
+          dataSource: response.data.data,
+          isLoading: false,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
   render() {
+    if (this.state.isLoading) {
+      return  <h1>LOADING</h1>
+    }
+
     return (
       <div>
           <h1>hey there</h1>
